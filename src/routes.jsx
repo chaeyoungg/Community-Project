695761b4957fe51b102851233f999f1da5beefb4
chaeyoungg <chaeyoungg77@gmail.com>
2024-07-09 09:48:35 +0900

    Docs : 컴포넌트 생성 및 분리, router 적용

---
+++ src/routes.jsx
@@ -0,0 +1,52 @@
+import Layout from "@components/layout/Layout";
+import Community from "@pages/community/Index";
+import Error from "@pages/Error";
+import Login from "@pages/user/Login";
+import Signup from "@pages/user/Signup";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <Error />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Community />,
+      },
+      // {
+      //   path: ":type",
+      //   element: <List />,
+      // },
+      // {
+      //   path: ":type/:_id",
+      //   element: <Detail />,
+      //   children: [
+      //     {
+      //       index: true,
+      //       element: <CommentList />,
+      //     },
+      //   ],
+      // },
+      // {
+      //   path: ":type/new",
+      //   element: <New />,
+      // },
+      // {
+      //   path: ":type/:_id/edit",
+      //   element: <Edit />,
+      // },
+      {
+        path: "user/login",
+        element: <Login />,
+      },
+      {
+        path: "user/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
+
+export default router;
