bea561a6f977975898b35fd7c4ce30b352b3b49e
chaeyoungg <chaeyoungg77@gmail.com>
2024-07-11 09:52:11 +0900

    Feat : 페이지네이션 기능

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,14 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
-const Pagination = ({ totalPage, current = 1 }) => {
+const Pagination = ({ totalPage, current = 1, setPage, type }) => {
   console.log(totalPage, current);
+  // const { type } = useParams();
+  const handleClick = (page) => {
+    setPage(page);
+  };
+
   let pageList = [];
   for (let page = 1; page <= totalPage; page++) {
-    pageList.push(
-      <li key={page} className="text-bold text-blue-700">
-        <Link to={`/info?page=${page}`}>{page}</Link>
-      </li>
-    );
+    if (current === page) {
+      pageList.push(
+        <button
+          key={page}
+          className="text-bold text-blue-700"
+          onClick={() => handleClick(page)}
+        >
+          <Link to={`/posts?type=${type}&limit=10&page=${page}`}>{page}</Link>
+        </button>
+      );
+    } else {
+      pageList.push(
+        <button
+          key={page}
+          className="text-bold white"
+          onClick={() => handleClick(page)}
+        >
+          <Link to={`/posts?type=${type}&limit=10&page=${page}`}>{page}</Link>
+        </button>
+      );
+    }
   }
   console.log("arr=>", pageList);
   return <ul className="flex justify-center gap-3 m-4">{pageList}</ul>;
