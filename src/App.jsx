ccbdab28c9d0bf87231e48dcf61eb086ab54a052
chaeyoungg <chaeyoungg77@gmail.com>
2024-07-11 11:18:29 +0900

    Feat : 다크모드 zustand 전역 상태 관리

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import { RouterProvider } from "react-router-dom";
 import router from "@/routes";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "react-query";
+import useThemeStore from "@zustand/themeStore";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
 function App() {
+  const { isDarkMode } = useThemeStore();
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
